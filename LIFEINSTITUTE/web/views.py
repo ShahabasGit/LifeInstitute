54bad2597110c2ef2d4ffd6bbb346bd93e8d1bee
ShahabasMM <166999989+ShahabasMM@users.noreply.github.com>
2024-07-05 22:07:53 +0530

    Add files via upload

---
+++ LIFEINSTITUTE/web/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from web.models import newsflash
+
+# Create your views here.
+
+def index (request):
+    flashnews = newsflash.objects.all()
+    context = {
+        "flashnews" : flashnews
+    }
+    return render(request, 'pages/index.html', context=context)
+
+def about (request):
+    flashnews = newsflash.objects.all()
+    context = {
+        "flashnews" : flashnews
+    }
+    return render(request, 'pages/about.html', context=context)
+
+def contact (request):
+    return render(request, 'pages/contact.html')
+
+def com_course (request):
+    return render(request, 'pages/com-courses.html')
+
+def dis_course (request):
+    return render(request, 'pages/dis-courses.html')
